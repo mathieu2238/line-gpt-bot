8ef91f2cb085fe6663705e7b13965734290098f6
mathieu2238 <matthew2238@gmail.com>
2025-04-17 16:30:46 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, abort
+import openai
+import os
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+app = Flask(__name__)
+
+# 環境変数
+openai.api_key = os.getenv("OPENAI_API_KEY")
+line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    user_msg = event.message.text
+
+    # ChatGPTへの問い合わせ
+    response = openai.ChatCompletion.create(
+        model="gpt-4",  # ここはgpt-4にしてもOK
+        messages=[{"role": "user", "content": user_msg}]
+    )
+    ai_reply = response.choices[0].message['content']
+
+    # LINEへ返信
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=ai_reply)
+    )
+
+if __name__ == "__main__":
+    app.run()
